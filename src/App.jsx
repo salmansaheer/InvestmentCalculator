22f0cd0c77d9614c6e3ffcd9675122525947972b
salmansaheer <salmanvk23yr@gmail.com>
2026-01-29 19:41:19 +0400

    Add investment calculator components and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
+import { useState } from 'react';
+import Header from './components/Header.jsx';
+import UserInput from './components/UserInput.jsx';
+import Results from './components/Results.jsx';
+
 function App() {
+  const [userInput, setUserInput] = useState({
+        initialInvestment: 100,
+        annualInvestment: 30,
+        expectedReturn: 10000,
+        duration: 3
+    })
+    function handleChange(inputIdentifier, value){
+        setUserInput((prevInput)=>{
+            return {
+                ...prevInput,
+                [inputIdentifier]: +value
+            }
+        })
+    }
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+      <Header />
+      <UserInput onChange={handleChange} userInput={userInput}/>
+      <Results input={userInput}/> 
+    </>
   )
 }
 
